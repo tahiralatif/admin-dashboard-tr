b7b40f11842ec50a569991909939d04abac858fd
tara rajput <tara378581@gmail.com>
2025-02-10 13:09:39 +0500

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import ProtectedRoutpage from './components/ProtectedRoute'
+'use client'
 
-const Homepage = () => {
+import Link from "next/link";
+import React from "react";
+
+const Page = () => {
   return (
-    <div>
-    </div>
-  )
-}
+    <div className="flex flex-col justify-center items-center h-screen bg-teal-400 text-white">
+      <h1 className="text-3xl font-bold mb-6">Welcome to Admin Dashboard!</h1>
 
-export default Homepage
+      <Link href="/admin">
+        <button className="transition duration-300 ease-in-out transform hover:scale-105 bg-white text-teal-600 font-semibold py-3 px-6 rounded-lg shadow-lg hover:bg-teal-500 hover:text-white">
+          Go to Login Dashboard
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default Page;
